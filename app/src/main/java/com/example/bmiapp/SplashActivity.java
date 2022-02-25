cda6bc12b8b61a9264b6bf00bcc0d696d8f82697
natnaelbiki <natnaelbiki@gmail.com>
2022-02-26 01:33:27 +0300

    first stage

---
+++ app/src/main/java/com/example/bmiapp/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.bmiapp;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
