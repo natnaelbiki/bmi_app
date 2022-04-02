380327ea8571e5607ee9cf866190ffb26ca3f739
natnaelbiki <natnaelbiki@gmail.com>
2022-04-02 16:41:18 +0300

    fragmentes refined 3

--- app/src/main/java/com/example/bmiapp/FirstFragment.java
+++ app/src/main/java/com/example/bmiapp/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.bmiapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -54,31 +56,29 @@ public class FirstFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 DecimalFormat df = new DecimalFormat("#.#");
+                try {
+                    if (txt_height.getText().toString().isEmpty()){
+                        txt_height.setBackgroundColor(Color.red(0));
+                        Toast.makeText(getActivity(), "Please fill height", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(txt_weight.getText().toString().isEmpty()) {
+                        txt_weight.setBackgroundColor(Color.red(0));
+                        Toast.makeText(getActivity(), "Please fill weight", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e) {
+                    Toast.makeText(getActivity(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
                 double height_in_meter = Double.parseDouble(String.valueOf(txt_height.getText()));
                 double weight_in_kg = Double.parseDouble(String.valueOf(txt_weight.getText()));
                 if (height_in_meter > 0 && weight_in_kg > 0) {
                     try {
-                        double bmi = weight_in_kg/(Math.pow(height_in_meter,2));
+                        double bmi = getBmi(weight_in_kg, height_in_meter);
                         bmi = Double.parseDouble(df.format(bmi));
-                        if (bmi<18.5) {
-                            txt_result_cat.setText("Underweight: you will have to gain more weight until you reach healthy levels");
-                            txt_result_bmi.setText("BMI: " + bmi);
-                        }
-                        else if (bmi>=18.5 && bmi<=24.9){
-                            txt_result_cat.setText("Healthy: Work to maintain this weight as you will significantly reduce your risks of developing health complications");
-                            txt_result_bmi.setText("BMI: " + bmi);
-                        }
-                        else if (bmi>=25 && bmi<=29.9){
-                            txt_result_cat.setText("Overweight: you will need to lose weight or you will be more susceptible to severe health complications");
-                            txt_result_bmi.setText("BMI: " + bmi);
-                        }
-                        else if (bmi>=30){
-                            txt_result_cat.setText("Obese: you will need to lose weight or you will be more susceptible to severe health complications");
-                            txt_result_bmi.setText("BMI: " + bmi);
-                        }
+                        txt_result_bmi.setText("BMI: "+bmi);
+                        txt_result_cat.setText(getCategory(bmi));
                     }catch (Exception e)
                     {
-                        txt_result_bmi.setText("error:"+(e.getMessage().toString()));
+                        Toast.makeText(getActivity(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                     }
 
                 }
@@ -90,6 +90,25 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    public double getBmi(double w, double h){
+        return w/(Math.pow(h,2));
+    }
+    public String getCategory(double bmi){
+        String s="";
+        if (bmi<18.5) {
+           s = ("Underweight: you will have to gain more weight until you reach healthy levels");
+        }
+        else if (bmi>=18.5 && bmi<=24.9){
+            s = ("Healthy: Work to maintain this weight as you will significantly reduce your risks of developing health complications");
+        }
+        else if (bmi>=25 && bmi<=29.9){
+            s = ("Overweight: you will need to lose weight or you will be more susceptible to severe health complications");
+        }
+        else if (bmi>=30){
+            s = ("Obese: you will need to lose weight or you will be more susceptible to severe health complications");
+        }
+        return s;
     }
 
 }
